6f1c0241c7692b0399158f669ea3c7c03ef42a12
nataliesmith32 <natalielsmith32@gmail.com>
2021-01-05 16:32:26 -0700

    schema update

--- schema.sql
+++ schema.sql
@@ -1,70 +1,58 @@
 DROP TABLE employee_data;
+DROP TABLE titles
+DROP TABLE manager
+DROP TABLE departments 
+DROP TABLE salaries 
+DROP TABLE emp_to_dept 
+DROP TABLE departments 
+
+SELECT * FROM departments
+
+CREATE TABLE departments (
+	dept_no VARCHAR (30),
+	dept_name VARCHAR (30),
+	PRIMARY KEY (dept_no)
+);
+
+SELECT * FROM manager
+
+CREATE TABLE manager (
+	dept_no VARCHAR (30),
+	emp_no VARCHAR (30)
+);
+
+SELECT * FROM emp_to_dept
+
+CREATE TABLE emp_to_dept (
+	emp_no VARCHAR (30),
+	dept_no VARCHAR (30)
+);
+
 
 SELECT * FROM employee_data;
 
 CREATE TABLE employee_data (
 	emp_no INT NOT NULL,
-	emp_title_id INT NOT NULL,
+	emp_title_id VARCHAR(30) ,
 	birth_date DATE,
 	first_name VARCHAR(30),
 	last_name VARCHAR(30),
 	sex VARCHAR(10),
-	hire_date DATE,
-	PRIMARY KEY (emp_no)
+	hire_date date,
+	CONSTRAINT pk_employees PRIMARY KEY (emp_no)
 );
-
-DROP TABLE titles
 
 SELECT * FROM titles
 
 CREATE TABLE titles (
-	title_id INT NOT NULL,
-	title VARCHAR(30),
-	PRIMARY KEY (title_id)
+	title_id VARCHAR(30),
+	title VARCHAR(30)
 );
-
-DROP TABLE salaries 
 
 SELECT * FROM salaries
 
 CREATE TABLE salaries (
-	emp_no INT NOT NULL,
-	salary INT NOT NULL,
-	PRIMARY KEY (emp_no)
+	emp_no VARCHAR(30),
+	salary INT NOT NULL
 );
-
-DROP TABLE emp_to_dept 
-
-SELECT * FROM emp_to_dept
-
-CREATE TABLE emp_to_dept (
-	emp_no INT NOT NULL,
-	dept_no INT NOT NULL,
-	PRIMARY KEY (emp_no)
-);
-
-DROP TABLE departments 
-
-SELECT * FROM departments
-
-CREATE TABLE departments (
-	dept_no INT NOT NULL,
-	dept_name VARCHAR (30),
-	PRIMARY KEY (dept_no)
-);
-
-DROP TABLE departments 
-
-SELECT * FROM departments
-
-CREATE TABLE departments (
-	dept_no INT NOT NULL,
-	dept_name VARCHAR (30),
-	PRIMARY KEY (dept_no)
-);
-
-DROP TABLE manager
-
-SELECT * FROM manager
-	dept_no INT NOT NULL,
-	emp_no INT NOT NULL
+	
